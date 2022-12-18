3f2052f459afab3c616a66f9ecf33eb8e5067d5f
Erick <santoserick9@gmail.com>
2022-12-18 19:31:08 -0300

    fix import name

--- src/application/entities/notification/notification.content.spec.ts
+++ src/application/entities/notification/notification.content.spec.ts
@@ -1,17 +1,19 @@
-import { Content } from './notification.content';
+import { NotificationContent } from './notification.content';
 
 describe('Notification Content', () => {
   it('should be able to create a notification content', () => {
-    const content = new Content('Você recebeu uma solicitação de amizade');
+    const content = new NotificationContent(
+      'Você recebeu uma solicitação de amizade',
+    );
 
     expect(content).toBeTruthy();
   });
 
   it('should not be able to create a notification content with less than 5 characteres', () => {
-    expect(() => new Content('a')).toThrow();
+    expect(() => new NotificationContent('a')).toThrow();
   });
 
   it('should not be able to create a notification content with more than 240 characteres', () => {
-    expect(() => new Content('a'.repeat(241))).toThrow();
+    expect(() => new NotificationContent('a'.repeat(241))).toThrow();
   });
 });
--- src/application/entities/notification/notification.spec.ts
+++ src/application/entities/notification/notification.spec.ts
@@ -1,10 +1,10 @@
 import { Notification } from './notification';
-import { Content } from './notification.content';
+import { NotificationContent } from './notification.content';
 
 describe('Notification', () => {
   it('should be able to create a notification content', () => {
     const notification = new Notification({
-      content: new Content('Nova solicitação de amizade'),
+      content: new NotificationContent('Nova solicitação de amizade'),
       category: 'social',
       recipientId: 'example-id',
     });
