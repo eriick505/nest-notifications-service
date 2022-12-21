d7c9adbe05658ad93ac50c76d6786561d47a9fae
Erick <santoserick9@gmail.com>
2022-12-21 10:06:45 -0300

    create mapper notification to domain

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,7 @@
+import { Notification as RawNotification } from '@prisma/client';
+
 import { Notification } from '@application/entities';
+import { NotificationContent } from '@application/entities/notification';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -11,5 +14,19 @@ export class PrismaNotificationMapper {
       createdAt: notification.createdAt,
       canceledAt: notification.canceledAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new NotificationContent(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
