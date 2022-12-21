79b9799499409c8653028850783b258878e86562
Erick <santoserick9@gmail.com>
2022-12-21 10:14:18 -0300

    move use cases from notification to notification folder

---
+++ src/application/use-cases/notification/index.ts
@@ -0,0 +1,6 @@
+export * from './send-notification';
+export * from './cancel-notification';
+export * from './count-recipient-notifications';
+export * from './get-recipient-notifications';
+export * from './read-notification';
+export * from './unread-notification';
