3786f0f35f650d180f094961486d9d19f2c9bf5d
Erick <santoserick9@gmail.com>
2022-12-21 09:26:19 -0300

    add use cases in export alias

--- src/application/use-cases/index.ts
+++ src/application/use-cases/index.ts
@@ -2,4 +2,6 @@ export * from './send-notification';
 export * from './cancel-notification';
 export * from './count-recipient-notifications';
 export * from './get-recipient-notifications';
+export * from './read-notification';
+export * from './unread-notification';
 export * from './errors/notification-not-found';
